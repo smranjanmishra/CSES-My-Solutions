beb3e9999dae3a11ef08698821e04d710a07fc61
smranjanmishra <soumyarmishra17@gmail.com>
2026-03-24 07:49:25 +0530

    Permutations fixing code

--- Sorting and Searching/Permutations.cpp
+++ Sorting and Searching/Permutations.cpp
@@ -1,13 +1,31 @@
-import java.util.*;
-public class Main{
-	public static void main(String[] args){
-		Scanner sc=new Scanner(System.in);
-		int n=sc.nextInt();
-		if(n==1)System.out.println(1);
-		else if(n<=3)System.out.println("NO SOLUTION");
-		else{
-			for(int i=2;i<=n;i+=2)System.out.print(i+" ");
-			for(int i=1;i<=n;i+=2)System.out.print(i+" ");
-		}
-	}
+#include <bits/stdc++.h>
+
+using namespace std;
+#define nl '\n'  
+#define sp ' '  
+
+void solve() {
+    int n;
+    cin >> n;
+    if (n == 1) {
+        cout << 1 << nl;
+    } 
+    else if (n <= 3) {
+        cout << "NO SOLUTION" << nl;
+    } 
+    else {
+        for (int i = 2; i <= n; i += 2) {
+            cout << i << sp;
+        }
+        for (int i = 1; i <= n; i += 2) {
+            cout << i << sp;
+        }
+    }
+}
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t = 1;
+    while (t--)
+        solve();
 }
