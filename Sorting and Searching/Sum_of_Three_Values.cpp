abd1e5d6d0f15e4cfa6970a49a7b14e934e5c791
smranjanmishra <soumyarmishra17@gmail.com>
2026-04-03 16:44:03 +0530

    Sum of Three Values

---
+++ Sorting and Searching/Sum_of_Three_Values.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define nl '\n'
+#define sp ' '
+#define ll long long
+#define vii vector<pair<int, int>>
+#define len(x) int((x).size())
+#define all(n) n.begin(), n.end()
+
+void solve() {
+    int n, x;
+    cin >> n >> x;
+
+    vii arr(n);
+    for (int i = 0; i < n; i++) {
+        int val;
+        cin >> val;
+        arr[i] = {val, i + 1};
+    }
+    sort(all(arr));
+    for (int i = 0; i < n - 2; i++) {
+        ll tsum = x - arr[i].first;
+        int j = i + 1, k = n - 1;
+        while (j < k) {
+            ll csum = arr[j].first + arr[k].first;
+            if (csum > tsum) {
+                k--;
+            } 
+            else if (csum < tsum) {
+                j++;
+            } 
+            else {
+                cout << arr[i].second << sp << arr[j].second << sp << arr[k].second << nl;
+                return;
+            }
+        }
+    }
+
+    cout << "IMPOSSIBLE" << nl;
+}
+int main() {
+    int t = 1;
+    while (t--)
+        solve();
+}
